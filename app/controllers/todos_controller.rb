53ce5b8caab028cbfc7f714c108c3cabd56cdb11
danniBr <danitza.br@gmail.com>
2018-07-26 17:01:49 -0400

    commit - create

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,8 +2,20 @@ class TodosController < ApplicationController
   def index
     @todos = Todo.all
   end
+
   def new
-  	@todo = Todo.new
+    @todo = Todo.new
   end
 
+  def create
+    @todo = Todo.new(todo_params)
+    @todo.save
+    redirect_to root_path
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:description, :completed)
+  end
 end
