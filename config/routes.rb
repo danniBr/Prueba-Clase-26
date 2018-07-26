0141bf36555a7f7600f442dff2ce91b22e5bac79
danniBr <danitza.br@gmail.com>
2018-07-26 19:57:36 -0400

    commit - list

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'todos', to: 'todos#index'
   get 'todos/new', to: 'todos#new'
+  get 'todos/list', to: 'todos#list'
   post 'todos', to: 'todos#create'
   get 'todos/:id', to: 'todos#show', as: 'todo'
   get 'todos/:id/edit', to: 'todos#edit', as: 'edit'
   patch 'todos/:id', to: 'todos#update'
   delete 'todos/:id', to: 'todos#destroy'
   get 'todos/:id/complete', to: 'todos#complete', as: 'complete'
-  root 'todos#index'
+  root 'todos#list'
 end
