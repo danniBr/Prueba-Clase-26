de726a0af5e6ea422e429b6f0354c9aa0d1c6756
danniBr <danitza.br@gmail.com>
2018-07-26 16:56:39 -0400

    commit - new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'todos', to: 'todos#index'
-  
+  get 'todos/new', to: 'todos#new'
   root 'todos#index'
 end
